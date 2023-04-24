35ffe897e570a6ee12851cf004cca0b7b5566f6d
Artem <tombrute54@gmail.com>
2023-04-24 17:48:33 +0300

    created model "Catalog"

---
+++ catalog-service/src/main/java/com/diploma/catalogservice/model/Catalog.java
@@ -0,0 +1,29 @@
+package com.diploma.catalogservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "catalog_services")
+public class Catalog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private Double price;
+}
+
+
