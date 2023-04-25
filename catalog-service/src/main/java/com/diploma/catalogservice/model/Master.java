bb0cc66e011224cf72d8a81bf64d9956ad88c001
Artem <tombrute54@gmail.com>
2023-04-25 16:14:47 +0300

    commit

--- catalog-service/src/main/java/com/diploma/catalogservice/model/Master.java
+++ catalog-service/src/main/java/com/diploma/catalogservice/model/Master.java
@@ -1,6 +1,5 @@
 package com.diploma.catalogservice.model;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,8 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "catalog_services")
-public class Catalog {
+@Table(name = "masters")
+public class Master {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,15 +19,7 @@ public class Catalog {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "description")
-    private String description;
+    @Column(name = "rating")
+    private int rating;
 
-    @Column(name = "price")
-    private Double price;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "master_id")
-    private Master master;
 }
-
-
