89f5325966501fb2149415651acb410e8237e6db
Artem <tombrute54@gmail.com>
2023-04-25 14:40:42 +0300

    commit

--- catalog-service/src/main/java/com/diploma/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/com/diploma/catalogservice/controller/CatalogController.java
@@ -1,11 +1,11 @@
 package com.diploma.catalogservice.controller;
 
+
 import com.diploma.catalogservice.model.Catalog;
 import com.diploma.catalogservice.service.CatalogService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +16,14 @@ public class CatalogController {
 
     private final CatalogService catalogService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Catalog createProduct(@RequestBody Catalog catalog) {
+       return catalogService.createProduct(catalog);
+    }
+
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Catalog> getAllServices() {
         return catalogService.getAllServices();
     }
--- catalog-service/src/main/java/com/diploma/catalogservice/service/CatalogService.java
+++ catalog-service/src/main/java/com/diploma/catalogservice/service/CatalogService.java
@@ -3,12 +3,14 @@ package com.diploma.catalogservice.service;
 import com.diploma.catalogservice.model.Catalog;
 import com.diploma.catalogservice.repository.CatalogRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CatalogService {
     private final CatalogRepository catalogRepository;
 
@@ -17,5 +19,12 @@ public class CatalogService {
 
 //        return products.stream().map(this::mapToProductResponse).toList();
         return catalogs.stream().map(catalog -> catalog).toList();
+    }
+
+
+    public Catalog createProduct(Catalog catalog) {
+
+        log.info("Product {} is saved", catalog.getId());
+        return   catalogRepository.save(catalog);
     }
 }
