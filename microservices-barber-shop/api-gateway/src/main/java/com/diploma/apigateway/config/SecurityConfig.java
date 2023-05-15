2c8131ac1aa0dcc8afb308d0418ca700ca67f72d
Artem <tombrute54@gmail.com>
2023-05-15 19:36:31 +0300

    security config / api

---
+++ microservices-barber-shop/api-gateway/src/main/java/com/diploma/apigateway/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.diploma.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+   @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//        http.authorizeExchange()
+//                .pathMatchers("/booking/**").hasRole("ADMIN")
+//                .pathMatchers("/catalog/**").authenticated()
+//                .pathMatchers("/feedback/**").authenticated()
+//                .pathMatchers("/schedule/**").hasAnyRole("ADMIN", "MASTER")
+//                .pathMatchers("/user/**").authenticated()
+//                .anyExchange().permitAll()
+//                .and().oauth2ResourceServer().jwt()
+//                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+//        return http.build();
+
+       http.authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
+                       .permitAll()
+                       .anyExchange()
+                       .authenticated()
+               )
+               .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+       return http.build();
+    }
+}
