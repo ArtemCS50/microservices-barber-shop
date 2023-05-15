2b26851a608a170e1d1e2bfa01b32a2ffb2387b6
Artem <tombrute54@gmail.com>
2023-05-15 21:35:59 +0300

    config discovery-server

---
+++ microservices-barber-shop/discovery-server/src/main/java/com/diploma/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.diploma.discoveryserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+//    @Override
+//    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder)throws Exception{
+//        authenticationManagerBuilder.inMemoryAuthentication()
+//                .withUser("eureka").password("password")
+//                .authorities("USER");
+//    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
+        return httpSecurity.build();
+    }
+}
